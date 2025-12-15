4bdab1943c9bb5e4c7cd98043a5bb082d616f81b
Raul Irus <raul.irus@gmail.com>
2025-12-15 15:30:25 +0100

    Add missing migration for TransactionCategory and schema sync

---
+++ prisma/migrations/20251215142844_ensure_schema_sync/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "TransactionCategory" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "color" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "TransactionCategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "TransactionCategory_name_key" ON "TransactionCategory"("name");
