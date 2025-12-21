585f80840e0c43cbdb74d0ed45c505aad9ce2080
Raul Irus <raul.irus@gmail.com>
2025-12-21 13:11:22 +0100

    chore: update seed.ts with latest local data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   console.log('🌱 Start seeding...');
-  console.log('Generated at: 2025-12-21T12:10:18.757Z');
+  console.log('Generated at: 2025-12-21T12:11:26.769Z');
 
   // --- CLEANUP (Delete existing data to enforce strict sync) ---
   console.log('🧹 Cleaning up existing data...');
