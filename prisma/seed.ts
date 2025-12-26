f99d9db42996bd13bd28037a7c22b06bd631b22f
Raul Irus <raul.irus@gmail.com>
2025-12-26 18:57:49 +0100

    .

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,7 +5,7 @@ const prisma = new PrismaClient();
 
 async function main() {
   console.log('🌱 Start seeding (Lossless Sync: CRM and System data)...');
-  console.log('Generated at: 2025-12-26T17:56:55.684Z');
+  console.log('Generated at: 2025-12-26T17:57:51.284Z');
 
   // --- 1. Transaction Categories ---
   console.log('Upserting Categories...');
